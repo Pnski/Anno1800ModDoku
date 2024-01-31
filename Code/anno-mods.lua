a5b53a45cc389ee275338889cf070b19c0cc0975
pnski <s1l3nce@hotmail.de>
2024-01-31 20:06:29 +0100

    u

--- Code/anno-mods.lua
+++ Code/anno-mods.lua
@@ -1,3 +1,5 @@
+_ttable = {} -- init empty table
+
 -- metatables have __index as a stored function, the absence of the function in a table shows its not a metatable
 local function check_mt(_table)
   if _table.__index ~= nil then
@@ -7,7 +9,7 @@ local function check_mt(_table)
   end
 end
 
-local function getChilds(_table)
+local function getkvtable(_table)
   if check_mt(_table) == true then
     return = getmetatable(_table)
   else
@@ -15,9 +17,43 @@ local function getChilds(_table)
   end
 end
 
-local function 
+local function resetTable()
+  _ttable = {}
+end
+
+local function getDeepTable(_table, int)
+  local int = int or 0
+  if type(_table) == 'table' then
+    _table = getkvtable(_table)
+    for k,v in pairs(_table) do
+      if type(v) == 'table' then
+        getDeepTable(k, int+1)
+      end
+    end
+  else
+    table.insert(_ttable, string.rep('\t', int)..tostring(k).." : "..tostring(v))
+  end
+end
+
+local function saveTableToFile(_table,fileName)
+  file = io.open(fileName,"a")
+  for i = 1,tableLength(_table) do
+    file:write(_table[i])
+  end
+  file:close()
+end
+
+function tableLength(T)
+  local count = 0
+  for _ in pairs(T) do
+    count = count + 1
+  end
+  return count
+end
+
 
 amods = {
-
+  getDeepTable = getDeepTable,
+  saveTable = saveTableToFile
 }
 print("anno-mods.lua loaded")
